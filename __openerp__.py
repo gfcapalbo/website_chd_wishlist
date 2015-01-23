b14ac7bef3dc0a2c606313ad9bfd580f81e8e406
gfcapalbo <giovanni@therp.nl>
2015-01-23 17:09:37 +0100

    [ADD] first commit

---
+++ __openerp__.py
@@ -0,0 +1,13 @@
+{
+'name': "website_chd_product_configurator",
+'description': """
+Web front for chindu product configurator
+""",
+'depends': ['base',
+            'website',
+            'website_chd_product_configurator'
+           ],
+'data': [
+
+        ],
+}
