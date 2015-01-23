6540b703bc1ccd3d0e0dbd9a2f0c97ad903ca9c6
gfcapalbo <giovanni@therp.nl>
2015-01-23 17:38:16 +0100

    [ADD] wishlist button added to options view

--- __openerp__.py
+++ __openerp__.py
@@ -1,13 +1,19 @@
 {
-'name': "website_chd_product_configurator",
+'name': "website_chd_wishlist",
 'description': """
-Web front for chindu product configurator
+Wishlist for web front chindu product configurator
+When the sales options appear, a button
+add to wishlist" will be available
+The user will have the possibility to view his wishlists and transform them in orders
+(in other words create quotations in the product configurator"
+
 """,
 'depends': ['base',
             'website',
+            'chd_product_configurator',
             'website_chd_product_configurator'
            ],
 'data': [
-
+        'templates/templates.xml',
         ],
 }
