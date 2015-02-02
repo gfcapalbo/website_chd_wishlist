d8d5b52ac226ee1d909d8668a25677f4598c8d5e
gfcapalbo <giovanni@therp.nl>
2015-02-02 16:29:10 +0100

    [FIX] managing empty wishlist case and also if partner does not exist

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -10,25 +10,33 @@ class wish_init(http.Controller):
     # display current users wishlist
     @http.route('/chd_init/wishlist/' ,website=True)
     def wish(self):
+         partner = self.get_current_partner()
+         if partner == False:
+             return  http.request.render('website_chd_product_configurator.no_partner',{
+                        })
          wishlist_model = http.request.env['chd.wishlist']
          result_model = http.request.env['chd.product_configurator.result']
          partner = self.get_current_partner()
+         if not partner:
+             return  http.request.render('website_chd_product_configurator.no_partner',{
+                        })
          wishlist = wishlist_model.search([('owner','=',partner.id)])
-         results = result_model.search([('wishlist','=',wishlist.ids[0])])
-
-         if results:
+         try:
+             results = result_model.search([('wishlist','=',wishlist.ids[0])])
              return  http.request.render('website_chd_wishlist.show_list',{
                        'user':partner,
                        'results':results,
                         })
-         else:
+         except:
              return  http.request.render('website_chd_wishlist.no_list',{
                        'user':partner,
                         })
 
-
     def get_current_partner(self):
          partner_model = http.request.env['res.partner']
          current_partner = partner_model.search([('user_account_id','=',http.request.uid)])
-         return current_partner[0]
+         if len(current_partner) == 0:
+             return False
+         else:
+             return current_partner.ids[0]
 
